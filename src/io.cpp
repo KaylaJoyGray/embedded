37e7f7a4e331be32f3b202a6cf212aaabb7702a3
Kayla Gray <kjoygray75@gmail.com>
2025-01-10 16:20:51 -0600

    Added ability to use backspace

--- src/io.cpp
+++ src/io.cpp
@@ -10,7 +10,18 @@ char STR[STRLEN + 1];
 
 const char *read_line() {
     char c, *ptr;
-    for (ptr = STR, c = getchar(); c != '\r' && ptr - STR < STRLEN; c = getchar()) putchar(*ptr++ = c);
+    for (ptr = STR, c = getchar(); c != '\r' && ptr - STR < STRLEN; c = getchar()) {
+        if (c != '\\') {
+            putchar(*ptr++ = c);
+        } else {
+            if (ptr >= &STR[0]) {
+                *ptr-- = 0;
+                putchar('\b');
+                putchar(' ');
+                putchar('\b');
+            }
+        }
+    }
     *ptr = 0;
     return STR;
 }
